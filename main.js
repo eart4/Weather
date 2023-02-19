f627594197df7a24a9fccb96a69511dcab2b9b47
eart4 <metrika987@gmail.com>
2023-02-19 18:23:28 +0300

    created function getWeather

--- main.js
+++ main.js
@@ -1,23 +1,7 @@
 const apiKey = 'ba916fb4e5924beab11125540231902'
-
-
-
-
-
 const query = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=London`
-
-// fetch(query).then((response) => {
-//     return response.json()
-// }).then((data) => {
-//     console.log(data)
-// })
-
-
-/* Получаем название города */
-
 const header = document.querySelector('.header')
 const form = document.querySelector('#form');
-
 const input = document.querySelector('#inputCity');
 
 function removeCard(){
@@ -25,23 +9,18 @@ function removeCard(){
     if (prevCard) prevCard.remove();
 }
 
-
-
 function showError(errorMessage){
     const html = `<div class="card">${data.error.message}</div>`;
-
     header.insertAdjacentHTML('afterend', html);
 }
 
-function showCard(name, country, temp_c, condition){
+function showCard({name , country, temp, condition}){
     const html = `<div class="card">
-
-
 <h2 class="card-city">${name}<span>${country}</span></h2>
 
 
 <div class="card-weather">
-  <div class="card-value">${temp_c}<sup>°c</sup></div>
+  <div class="card-value">${temp}<sup>°c</sup></div>
   <img class="card-img" src="./images/example.svg" alt="">
 </div>
 
@@ -50,58 +29,50 @@ function showCard(name, country, temp_c, condition){
 
 
 </div>`;
-// отображаем карточку на стр
 
 header.insertAdjacentHTML('afterend', html);
 }
 
-/* Слушаем отправку формы */
+async function getWeather(city){
+    const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
 
 
-form.onsubmit = function (e) {
+
+
+
+form.onsubmit = async function (e) {
     // отменяем отправку формы
     e.preventDefault();
 
     // берем значение импута обрезаем пробулы
 
     let city = input.value.trim();
-
-    //делакм запрос на сервер
-    const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
-
-    //выполняем запрос
-    fetch(url).then((response) => {
-        return response.json();
-    }).then((data) => {
-
-        // проверка на ошибку
-        if (data.error) {
-
-            removeCard();
-            showError(data.error.message);
+    const data = await getWeather(city)
 
 
+    
+if (data.error) {
+    removeCard();
+    showError(data.error.message);
+
+} else {
+    removeCard();
+
+    const weatherData = {
+        name: data.location.name, 
+        country: data.location.country, 
+        temp:data.current.temp_c,
+        condition :data.current.condition.text
+    }
+    showCard(weatherData);
+}
 
 
-        } else {
 
-            removeCard();
-            // разметка для карточки
-            showCard(
-                data.location.name, 
-                data.location.country, 
-                data.current.temp_c,
-                data.current.condition.text
-            );
+ 
 
-
-            }
-            
-
-        console.log(data);
-
-        // удаляем старыекарточки
-
-
-    });
 }
